0467627ff11c9b788488e4cd1da07b197dd35659
swaga32 <jacksonmbirui@gmail.com>
2022-07-01 17:51:38 -0700

    CV92

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,16 +2,19 @@
 
 /**
  * main - Prints the alphabet in lowercase, except for q and e.
+ *
  * Return: Always 0.
  */
 int main(void)
 {
+
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-         	if (letter != 'e' && letter != 'q')
-			putchar(letter);
+		if (letter != 'e' && letter != 'q')
+
+		        putchar(letter);
 	}
 
 	putchar('\n');
