1a85466b80c185ccd662f5cb532d9b670494d8d1
swaga32 <jacksonmbirui@gmail.com>
2022-07-05 03:25:45 -0700

    loop32

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,12 +5,12 @@
 */
 void print_alphabet(void)
 {
-       int i;
+int i;
 
-       for (i = 97; i <= 122; i++)
-       {
-		_putchar(i);
-       }
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
 
-        _putchar('\n');
+_putchar('\n');
 }
