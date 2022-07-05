709009a7371feb084e3e7711445044dd0336adee
swaga32 <jacksonmbirui@gmail.com>
2022-07-05 02:58:20 -0700

    loop fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- * main - Prints "_putchar" followed by a new line.
- *
- * Return: Always 0.
- */
+* main - Prints "_putchar" followed by a new line.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
         _putchar('_');
