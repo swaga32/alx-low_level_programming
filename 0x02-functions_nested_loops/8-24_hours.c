d1f513e8c81f8d5eca1f5d252f6da266fe622629
swaga32 <jacksonmbirui@gmail.com>
2022-07-05 16:12:37 -0700

    loopfrv

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * jack_bauer - Prints every minute of the day of
  *              Jack Bauer, starting from 00:00 to 23:59.
@@ -10,7 +9,7 @@ void jack_bauer(void)
 
 	for (hour = 0; hour <= 23; hour++)
 	{
-	for (minute = 0; minute <= 59; minute++)
+		for (minute = 0; minute <= 59; minute++)
 		{
 			_putchar((hour / 10) + '0');	
 			_putchar((hour % 10) + '0');
