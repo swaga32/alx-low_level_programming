49cb63c229e72d72c286745a7b45ed404efbc23f
swaga32 <jacksonmbirui@gmail.com>
2022-07-05 05:15:07 -0700

    loop98

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 	int a, b;
 
