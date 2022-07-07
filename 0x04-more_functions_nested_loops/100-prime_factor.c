5cac10b6ebbb20669f7d922cecb03e43b262f79b
swaga32 <jacksonmbirui@gmail.com>
2022-07-07 03:12:01 -0700

    nestloop s

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,8 +8,8 @@
 
 int main(void)
 {
-
 	long prime = 612852475143, div;
+
 	while (div < (prime / 2))
 	{
 		if ((prime % 2) == 0)
@@ -17,12 +17,15 @@ int main(void)
 			prime /= 2;
 			continue;
 		}
+
 		for (div = 3; div < (prime / 2); div += 2)
 		{
 			if ((prime % div) == 0)
 				prime /= div;
 		}
 	}
+
 	printf("%ld\n", prime);
+
 	return (0);
 }
