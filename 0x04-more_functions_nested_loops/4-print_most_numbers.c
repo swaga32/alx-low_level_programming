de27e7e1587510038d608fca312b40206f1530e0
swaga32 <jacksonmbirui@gmail.com>
2022-07-07 02:39:55 -0700

    loopde

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,17 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_most_numbers - Prints the numbers from 0-9 except for 2 and 4.
+ * print_most_numbers - prints numbers between 0 to 9
+ * (neither 2 nor 4).
+ * Return: no return.
  */
-
 void print_most_numbers(void)
 {
-	int num;
-	for (num = 0; num <= 9; num++)
+	int ch;
+	for (ch = 48; ch < 58; ch++)
 	{
-		if (num != 2 && num != 4)
-			_putchar((num % 10) + '0');
+		if (ch != 50 && ch != 52)
+			_putchar(ch);
 	}
-
 	_putchar('\n');
 }
