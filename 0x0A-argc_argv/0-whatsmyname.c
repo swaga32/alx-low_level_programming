17674cdd6c5ede4cf3023389ab3f66a9d178f121
swaga32 <jacksonmbirui@gmail.com>
2022-07-22 02:10:48 -0700

    argnj

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
  * @argv: an array of a command listed
  * Return: 0 for success
  */
-int main (int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", argv[0]);
 	return (0);
 }
