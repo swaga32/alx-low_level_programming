3d1fe224f5d660308ca08a7eaaea29b52a81e798
swaga32 <jacksonmbirui@gmail.com>
2022-08-03 03:23:24 -0700

    poin

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,7 @@
 #include "3-calc.h"
 
 /**
- * main - check the code for Holberton School students.
+ * main - check the code.
  * @argc: argument count.
  * @argv: argument vector.
  * Return: Always 0.
