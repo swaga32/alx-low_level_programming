e361b2e59d14ab3292a577a5b9dfca5cce8397dd
swaga32 <jacksonmbirui@gmail.com>
2022-08-22 04:09:10 -0700

    filling

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,43 @@
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * read_textfile - Reads a textfile and prints it to the POSIX standard output.
- * @filename: The filename.
- * @letters: The number of letters to read and print.
- * Return: The actual number of letters it could read and print if everything
- *	   runs well. 0 otherwise.
+ * read_textfile - check the code for Holberton School students.
+ * @filename: file to read and write
+ * @letters: number of letters to read and write.
+ * Return: letters printed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
-	int fd, tmp = 1;
-	size_t read_size = 0;
+	ssize_t nletters;
+	int file;
+	char *text;
 
-	printf("la taille est %ld", letters);
 	if (!filename)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if(fd == -1)
+	text = malloc(sizeof(char) * letters + 1);
+	if (text == NULL)
 		return (0);
-	buffer = malloc(letters * sizeof(char));
-	buffer[letters] = '\0';
-	do {
-		tmp = read(fd, buffer, letters);
-		read_size += tmp;
-	} while (tmp); 
-	close(fd);
-	if (read_size != letters)
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(text);
 		return (0);
-	write(1, buffer, letters);
-	return (read_size)
+	}
+	nletters = read(file, text, sizeof(char) * letters);
+	if (nletters == -1)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
+	nletters = write(STDOUT_FILENO, text, nletters);
+	if (nletters == -1)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
+	free(text);
+	close(file);
+	return (nletters);
 }
