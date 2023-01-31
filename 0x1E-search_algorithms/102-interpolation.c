59c6e45c0904036ef029dee66d7b4a0356e71ac2
root <jacksonmbirui3@gmail.com>
2023-01-31 03:21:49 -0800

    huh

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -7,7 +7,7 @@
  */
 void print_i(int *array, size_t i)
 {
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 }
 
 /**
@@ -19,47 +19,27 @@ void print_i(int *array, size_t i)
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-		size_t l = 0;
-		size_t r = size - 1;
-		size_t pos;
+	size_t l = 0;
+	size_t r = size - 1;
+	size_t pos;
 
-		if (array == NULL)
-			return (-1);
+	if (array == NULL)
+		return (-1);
 
-
-
-						pos = l + (((double)(r - l) / (array[r] - array[l]))
-
-										   * (value - array[l]));
-						*
-							* 	while (pos < size)
-							*
-							* 		{
-								*
-									* 				print_i(array, pos);
-								*
-									* 						if (array[pos] == value)
-									*
-									* 									return (pos);
-								*
-									* 											else if (array[pos] < value)
-									*
-									* 														l = pos + 1;
-								*
-									* 																else
-									*
-									* 																			r = pos - 1;
-								*
-									* 																					pos = l + (((double)(r - l) / (array[r] - array[l]))
-											*
-											* 																								   * (value - array[l]));
-								*
-									* 																								   	}
-						*
-							* 																								   		printf("Value checked array[%lu] is out of range\n", pos);
-						*
-							* 																								   			return (-1);
-						*
-							* 																								   			}
-							*
-							* 																								   			
+	pos = l + (((double)(r - l) / (array[r] - array[l]))
+			* (value - array[l]));
+	while (pos < size)
+	{
+		print_i(array, pos);
+		if (array[pos] == value)
+			return (pos);
+		else if (array[pos] < value)
+			l = pos + 1;
+		else
+			r = pos - 1;
+		pos = l + (((double)(r - l) / (array[r] - array[l]))
+				* (value - array[l]));
+	}
+	printf("Value checked array[%lu] is out of range\n", pos);
+	return (-1);
+}
